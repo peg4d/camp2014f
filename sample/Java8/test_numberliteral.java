4f5aa3295310fbbd77a2a3a689fc625701601bb0
Kouhei-Moriya <filterplant.moriya@gmail.com>
2014-11-20 14:22:00 +0900

    support binary and octal literal, qualified name, and instance creation of inner class bug fixed of method call and method reference

--- sample/Java8/test_numberliteral.java
+++ sample/Java8/test_numberliteral.java
@@ -5,6 +5,6 @@ import eee.fff.ggg;
 public final static class Hoge<T> extends Hogehoge {
 	public int[] a = {99_99_99, 0xFFEF_B3B0, 100};
 	public int b[][] = new int[256][256];
-	//public int e = 07_77
-	//public int f = 0b0110_10__11_1110;
+	public int e = 0_77_7;
+	public int f = 0b0110_10__11_1110;
 }
